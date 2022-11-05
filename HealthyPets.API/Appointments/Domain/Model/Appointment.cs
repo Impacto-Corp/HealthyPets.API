696be8afb992b97d852a64cd415962a14384cdf0
bruno1923 <u201914974@upc.edu.pe>
2022-11-05 14:56:44 -0500

    fix(developer): fixed error

--- HealthyPets.API/Appointments/Domain/Model/Appointment.cs
+++ HealthyPets.API/Appointments/Domain/Model/Appointment.cs
@@ -6,7 +6,7 @@ namespace HealthyPets.API.Appointments.Domain.Model;
 public class Appointment
 {
     public int Id { get; set; }
-    public Person Client { get; set; }
+    public Client Client { get; set; }
     public Pets Pet { get; set; }
     
 }
