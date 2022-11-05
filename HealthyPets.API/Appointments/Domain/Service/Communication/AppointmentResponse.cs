87945447623964c45ad3f3e1a75bf657fa6bab1e
RauVR <u201714049@upc.edu.pe>
2022-11-05 17:11:58 -0500

    feat(AppointmentResponse): Class AppointmentResponse implemented

---
+++ HealthyPets.API/Appointments/Domain/Service/Communication/AppointmentResponse.cs
@@ -0,0 +1,15 @@
+﻿using HealthyPets.API.Appointments.Domain.Model;
+using HealthyPets.API.Shared.Domain.Services.Communication;
+
+namespace HealthyPets.API.Appointments.Domain.Service.Communication;
+
+public class AppointmentResponse : BaseResponse<Appointment>
+{
+    public AppointmentResponse(string message) : base(message)
+    {
+    }
+
+    public AppointmentResponse(Appointment resource) : base(resource)
+    {
+    }
+}
