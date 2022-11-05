dc952df87b298545200c630ac9d6ac2a5fab1642
RauVR <u201714049@upc.edu.pe>
2022-11-05 16:17:46 -0500

    Chore: IAppointmentRepository Class Created and Implemented

---
+++ HealthyPets.API/Appointments/Domain/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealthyPets.API.Appointments.Domain.Model;
+
+namespace HealthyPets.API.Appointments.Domain.Repositories;
+
+public interface IAppointmentRepository
+{
+    Task<IEnumerable<Appointment>> ListAsync();//darme todos
+    Task AddAsync(Appointment appointment);//Agregar uno
+    Task<Appointment> FindByIdAsync(int id);//Buscar uno por Id
+    void Update(Appointment appointment);//Actualizar
+    void Remove(Appointment appointment);//Eliminar
+}
