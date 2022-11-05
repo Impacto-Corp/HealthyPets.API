858eee3f96dc1b470003f29a0ef7d7c6428d5942
RauVR <u201714049@upc.edu.pe>
2022-11-05 17:16:27 -0500

    feat(IAppointmentService): Interface IAppointmentService implemented

---
+++ HealthyPets.API/Appointments/Domain/Service/IAppointmentService.cs
@@ -0,0 +1,12 @@
+﻿using HealthyPets.API.Appointments.Domain.Model;
+using HealthyPets.API.Appointments.Domain.Service.Communication;
+
+namespace HealthyPets.API.Appointments.Domain.Service;
+
+public interface IAppointmentService
+{
+    Task<IEnumerable<Appointment>> ListAsync();
+    Task<AppointmentResponse> SaveAsync(Appointment appointment);
+    Task<AppointmentResponse> UpdateAsync(int id, Appointment appointment);
+    Task<AppointmentResponse> DeleteAsync(int id);
+}
