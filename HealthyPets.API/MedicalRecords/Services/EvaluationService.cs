3f085120875212d9c634dfcf40b5da3cb730d31d
Lynn Jeerferzon <81722082+LynnJ-Meza@users.noreply.github.com>
2022-11-05 16:43:41 -0500

    feat(Evaluation): implemented evaluation services.

---
+++ HealthyPets.API/MedicalRecords/Services/EvaluationService.cs
@@ -0,0 +1,52 @@
+﻿using HealthyPets.API.MedicalRecords.Domain.Models;
+using HealthyPets.API.MedicalRecords.Domain.Repositories;
+using HealthyPets.API.MedicalRecords.Domain.Services;
+using HealthyPets.API.MedicalRecords.Domain.Services.Communication;
+using HealthyPets.API.Shared.Domain.Repositories;
+
+namespace HealthyPets.API.MedicalRecords.Services;
+
+public class EvaluationService:IEvaluationService
+{
+    private readonly IEvaluationRepository _evaluationRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public EvaluationService(IUnitOfWork unitOfWork, IEvaluationRepository evaluationRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _evaluationRepository = evaluationRepository;
+    }
+
+    public async Task<IEnumerable<Evaluation>> ListAsync()
+    {
+        return await _evaluationRepository.ListAsync();
+    }
+
+    public async Task<EvaluationResponse> SaveAsync(Evaluation evaluation)
+    {
+        try
+        {
+            await _evaluationRepository.AddAsync(evaluation);
+            await _unitOfWork.CompleteAsync();
+            return new EvaluationResponse(evaluation);
+        }
+        catch (Exception e)
+        {
+            return new EvaluationResponse($"An error occurred while saving the evaluation:{e.Message}");
+        }
+    }
+
+    public async Task<EvaluationResponse> UpdateAsync(int id, Evaluation evaluation)
+    {
+        var existingEvaluation = await _evaluationRepository.FindByIdAsync(id);
+        if ()
+        {
+            
+        }
+    }
+
+    public Task<EvaluationResponse> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
