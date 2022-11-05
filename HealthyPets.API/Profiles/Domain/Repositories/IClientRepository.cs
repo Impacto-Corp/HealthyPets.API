395661d472fb5a8169727bf9645ba3b7578f1996
bruno1923 <u201914974@upc.edu.pe>
2022-11-05 14:46:43 -0500

    feat(Profiles): added Client repository.

---
+++ HealthyPets.API/Profiles/Domain/Repositories/IClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+
+namespace HealthyPets.API.Profiles.Domain.Repositories;
+
+public interface IPersonsRepository
+{
+    Task<IEnumerable<Client>> ListAsync();
+    Task AddAsync(Client client);
+    Task<Client> FindByIdAsync(int id);
+    void Update(Client client);
+    void Remove(Client client);
+}
