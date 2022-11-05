fa7b1a60d6e5660cfcd6aad9f46cf449eae460b5
Lynn Jeerferzon <81722082+LynnJ-Meza@users.noreply.github.com>
2022-11-05 17:20:06 -0500

    feat(Evaluation): added initial action for evaluations controller.

---
+++ HealthyPets.API/MedicalRecords/Controllers/EvaluationsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mime;
+using HealthyPets.API.MedicalRecords.Domain.Models;
+using HealthyPets.API.MedicalRecords.Domain.Services;
+using HealthyPets.API.MedicalRecords.Domain.Services.Communication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyPets.API.MedicalRecords.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class EvaluationsController:ControllerBase
+{
+    private readonly IEvaluationService _evaluationService;
+
+    public EvaluationsController(IEvaluationService evaluationService)
+    {
+        _evaluationService = evaluationService;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<Evaluation>> GetAllAsync()
+    {
+        var evaluations = await _evaluationService.ListAsync();
+        return evaluations;
+    }
+    
+}
