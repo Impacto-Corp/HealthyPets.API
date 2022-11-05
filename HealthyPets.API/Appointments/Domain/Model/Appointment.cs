69ea4afc07803369abfd398eb442dc7bb3a52634
RauVR <u201714049@upc.edu.pe>
2022-11-05 12:45:58 -0500

    feat: Appointment Class implemented

--- HealthyPets.API/Appointments/Domain/Model/Appointment.cs
+++ HealthyPets.API/Appointments/Domain/Model/Appointment.cs
@@ -5,6 +5,7 @@ namespace HealthyPets.API.Appointments.Domain.Model;
 public class Appointment
 {
     public int Id { get; set; }
+    public DateOnly Date { get; set; }
     public Person Client { get; set; }
     public Pets Pet { get; set; }
     
