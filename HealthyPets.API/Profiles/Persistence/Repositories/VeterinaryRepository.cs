bebf2a71af7f9ab0b4b6b853201c29b36f7026c3
bruno1923 <u201914974@upc.edu.pe>
2022-11-06 14:56:31 -0500

    feature(Profiles): Created VeterinaryRepository.cs

---
+++ HealthyPets.API/Profiles/Persistence/Repositories/VeterinaryRepository.cs
@@ -0,0 +1,39 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+using HealthyPets.API.Profiles.Domain.Repositories;
+using HealthyPets.API.Shared.Persistence.Contexts;
+using HealthyPets.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthyPets.API.Profiles.Persistence.Repositories;
+
+public class VeterinaryRepository : BaseRepository, IVeterinaryRepository
+{
+    public VeterinaryRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Veterinary>> ListAsync()
+    {
+        return await _context.Veterinaries.ToListAsync();
+    }
+
+    public async Task AddAsync(Veterinary veterinary)
+    {
+        await _context.Veterinaries.AddAsync(veterinary);    
+    }
+
+    public async Task<Veterinary> FindByIdAsync(int id)
+    {
+        return await _context.Veterinaries.FindAsync(id);
+    }
+
+    public void Update(Veterinary veterinary)
+    {
+        _context.Veterinaries.Update(veterinary);
+    }
+
+    public void Remove(Veterinary veterinary)
+    {
+        _context.Veterinaries.Remove(veterinary);
+    }
+}
