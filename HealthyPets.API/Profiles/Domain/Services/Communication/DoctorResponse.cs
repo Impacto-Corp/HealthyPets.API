e5f0f342f53302475290e2f96b6a134704c95834
bruno1923 <u201914974@upc.edu.pe>
2022-11-06 01:17:10 -0500

    feature(Profiles): Created DoctorResponse.cs

---
+++ HealthyPets.API/Profiles/Domain/Services/Communication/DoctorResponse.cs
@@ -0,0 +1,15 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+using HealthyPets.API.Shared.Domain.Services.Communication;
+
+namespace HealthyPets.API.Profiles.Domain.Services.Communication;
+
+public class DoctorResponse: BaseResponse<Doctor>
+{
+    public DoctorResponse(string message) : base(message)
+    {
+    }
+
+    public DoctorResponse(Doctor resource) : base(resource)
+    {
+    }
+}
