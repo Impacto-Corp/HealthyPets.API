9f62aace4193d7270388da79161c8093a965aed1
Markollantes2307 <mrcntn6991@gmail.com>
2022-11-05 20:44:46 -0500

    feat: Communications & Pet Service Updated

--- HealthyPets.API/Patients/Domain/Services/IPetsService.cs
+++ HealthyPets.API/Patients/Domain/Services/IPetsService.cs
@@ -1,8 +1,12 @@
 ﻿using HealthyPets.API.Patients.Domain.Model;
+using HealthyPets.API.Patients.Domain.Services.Communications;
 
 namespace HealthyPets.API.Patients.Domain.Services;
 
 public interface IPetsService
 {
     Task<IEnumerable<Pet>> ListAsync();
+    Task<PetResponse> SaveAsync(Pet pet);
+    Task<PetResponse> UpdateAsync(int id, Pet pet);
+    Task<PetResponse> DeleteAsync(int id);
 }
