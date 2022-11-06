d81e243b170d55c817047c672cb1be782a7dbc7c
RauVR <u201714049@upc.edu.pe>
2022-11-05 22:43:07 -0500

    update(Appointment): Class Program updated

--- HealthyPets.API/Program.cs
+++ HealthyPets.API/Program.cs
@@ -1,3 +1,12 @@
+using HealthyPets.API.Appointments.Domain.Repositories;
+using HealthyPets.API.Appointments.Domain.Service;
+using HealthyPets.API.Appointments.Persistence.Repositories;
+using HealthyPets.API.Appointments.Services;
+using HealthyPets.API.Shared.Domain.Repositories;
+using HealthyPets.API.Shared.Persistence.Contexts;
+using HealthyPets.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Add Database Connection
+
+var connectionString = builder.Configuration
+    .GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+
+builder.Services.AddAutoMapper(
+    typeof(HealthyPets.API.Appointments.Mapping.ModelToResourceProfile),
+    typeof(HealthyPets.API.Appointments.Mapping.ResourceToModelProfile));
+
+
+
 
 var app = builder.Build();
 
