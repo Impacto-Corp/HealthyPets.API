40a2ad4c6a6fb297477b2ab7dd3596ab111681a2
bruno1923 <u201914974@upc.edu.pe>
2022-11-06 00:48:11 -0500

    feature(Profiles): update Persistence

---
+++ HealthyPets.API/Profiles/Persistence/Repositories/ClientRepository.cs
@@ -0,0 +1,39 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+using HealthyPets.API.Profiles.Domain.Repositories;
+using HealthyPets.API.Shared.Persistence.Contexts;
+using HealthyPets.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthyPets.API.Profiles.Persistence.Repositories;
+
+public class ClientRepository:BaseRepository, IClientRepository
+{
+    public ClientRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public Task<IEnumerable<Client>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Client> FindByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Client client)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ HealthyPets.API/Profiles/Persistence/Repositories/DoctorRepository.cs
@@ -0,0 +1,38 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+using HealthyPets.API.Profiles.Domain.Repositories;
+using HealthyPets.API.Shared.Persistence.Contexts;
+using HealthyPets.API.Shared.Persistence.Repositories;
+
+namespace HealthyPets.API.Profiles.Persistence.Repositories;
+
+public class DoctorRepository : BaseRepository, IDoctorRepository
+{
+    public DoctorRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public Task<IEnumerable<Doctor>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Doctor doctor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Doctor> FindByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Doctor doctor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Doctor doctor)
+    {
+        throw new NotImplementedException();
+    }
+}
