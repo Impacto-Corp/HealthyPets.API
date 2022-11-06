ca47582b3c1ae849301e2fe606ad6abd224ac2e5
bruno1923 <u201914974@upc.edu.pe>
2022-11-06 14:58:18 -0500

    feature(Profiles): Created VeterinaryResponse.cs

---
+++ HealthyPets.API/Profiles/Domain/Services/Communication/VeterinaryResponse.cs
@@ -0,0 +1,15 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+using HealthyPets.API.Shared.Domain.Services.Communication;
+
+namespace HealthyPets.API.Profiles.Domain.Services.Communication;
+
+public class VeterinaryResponse: BaseResponse<Veterinary>
+{
+    public VeterinaryResponse(string message) : base(message)
+    {
+    }
+
+    public VeterinaryResponse(Veterinary resource) : base(resource)
+    {
+    }
+}
