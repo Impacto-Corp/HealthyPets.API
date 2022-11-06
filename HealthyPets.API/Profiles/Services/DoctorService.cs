4397639cb7f849b0cb9a244066e63c0916b363cd
bruno1923 <u201914974@upc.edu.pe>
2022-11-06 02:27:34 -0500

    feature(Profiles): update

---
+++ HealthyPets.API/Profiles/Services/DoctorService.cs
@@ -0,0 +1,28 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+using HealthyPets.API.Profiles.Domain.Services;
+using HealthyPets.API.Profiles.Domain.Services.Communication;
+
+namespace HealthyPets.API.Profiles.Services;
+
+public class DoctorService: IDoctorServices
+{
+    public Task<IEnumerable<Doctor>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DoctorResponse> SaveAsync(Doctor doctor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DoctorResponse> UpdateAsync(int id, Doctor doctor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DoctorResponse> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
