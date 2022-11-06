e0aa9317e04b342ec415efa0f3dff568a4daf860
bruno1923 <u201914974@upc.edu.pe>
2022-11-06 14:34:45 -0500

    feature(Profiles): Created IVeterinaryServices

---
+++ HealthyPets.API/Profiles/Domain/Services/IVeterinaryService.cs
@@ -0,0 +1,12 @@
+﻿using HealthyPets.API.Profiles.Domain.Model;
+using HealthyPets.API.Profiles.Domain.Services.Communication;
+
+namespace HealthyPets.API.Profiles.Domain.Services;
+
+public interface IVeterinaryService
+{
+    Task<IEnumerable<Veterinary>> ListAsync();
+    Task<VeterinaryResponse> SaveAsync(Veterinary veterinary);
+    Task<VeterinaryResponse> UpdateAsync(int id, Veterinary veterinary);
+    Task<VeterinaryResponse> DeleteAsync(int id);
+}
