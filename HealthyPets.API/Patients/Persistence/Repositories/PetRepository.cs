9b928ac235e13d568fd392a519c82a2e632e77cc
Markollantes2307 <mrcntn6991@gmail.com>
2022-11-05 20:13:27 -0500

    feat: Pet Persistence Repository

---
+++ HealthyPets.API/Patients/Persistence/Repositories/PetRepository.cs
@@ -0,0 +1,39 @@
+﻿using HealthyPets.API.Patients.Domain.Model;
+using HealthyPets.API.Patients.Domain.Repositories;
+using HealthyPets.API.Shared.Persistence.Contexts;
+using HealthyPets.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthyPets.API.Patients.Persistence.Repositories;
+
+public class PetRepository : BaseRepository, IPetsRepository
+{
+    public PetRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Pet>> ListAsync()
+    {
+        return await _context.Pets.ToListAsync();
+    }
+
+    public async Task AddAsync(Pet pet)
+    {
+        await _context.Pets.AddAsync(pet);
+    }
+
+    public async Task<Pet> FindByIdAsync(int id)
+    {
+        return await _context.Pets.FindAsync(id);
+    }
+
+    public void Update(Pet pet)
+    {
+        _context.Pets.Update(pet);
+    }
+
+    public void Remove(Pet pet)
+    {
+        _context.Pets.Remove(pet);
+    }
+}
