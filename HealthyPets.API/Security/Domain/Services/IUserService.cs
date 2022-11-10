3593d6ba5d211cfc8084272119a11ca73acbee74
RauVR <u201714049@upc.edu.pe>
2022-11-10 16:42:54 -0500

    feat(Security): Interface IUserService implemented.

---
+++ HealthyPets.API/Security/Domain/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using HealthyPets.API.Security.Domain.Model;
+using HealthyPets.API.Security.Domain.Services.Communication;
+
+namespace HealthyPets.API.Security.Domain.Services;
+
+public interface IUserService
+{
+    Task<AuthenticateResponse> AuthenticateAsync(AuthenticateRequest model);
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetByIdAsync(int id);
+    Task RegisterAsync(RegisterRequest model);
+    Task UpdateAsync(int id, UpdateRequest model);
+    Task DeleteAsync(int id);
+}
