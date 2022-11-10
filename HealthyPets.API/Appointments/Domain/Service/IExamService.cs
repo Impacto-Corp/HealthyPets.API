ea605517349f25f83ecdd128e3f282ed9f099ce7
RauVR <u201714049@upc.edu.pe>
2022-11-10 16:40:25 -0500

    feat(Appointment): Class IExamService implemented.

---
+++ HealthyPets.API/Appointments/Domain/Service/IExamService.cs
@@ -0,0 +1,12 @@
+﻿using HealthyPets.API.Appointments.Domain.Model;
+using HealthyPets.API.Appointments.Domain.Service.Communication;
+
+namespace HealthyPets.API.Appointments.Domain.Service;
+
+public interface IExamService
+{
+    Task<IEnumerable<Exam>> ListAsync();
+    Task<ExamResponse> SaveAsync(Exam exam);
+    Task<ExamResponse> UpdateAsync(int id, Exam exam);
+    Task<ExamResponse> DeleteAsync(int id);
+}
