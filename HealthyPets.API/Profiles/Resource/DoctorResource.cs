36eff09c164aea07ca24ae8f072c7d1ca244c335
bruno1923 <u201914974@upc.edu.pe>
2022-11-16 12:36:10 -0500

    feat(profiles): update resources

--- HealthyPets.API/Profiles/Resource/DoctorResource.cs
+++ HealthyPets.API/Profiles/Resource/DoctorResource.cs
@@ -4,4 +4,9 @@ public class DoctorResource
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public long Phone { get; set; }
 }
--- HealthyPets.API/Profiles/Resource/SaveDoctorResource.cs
+++ HealthyPets.API/Profiles/Resource/SaveDoctorResource.cs
@@ -6,4 +6,13 @@ public class SaveDoctorResource
 {
     [Required]
     public string Name { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string LastName { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Email { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Phone { get; set; }
 }
