7fd16c959816d8c9fe988c4e0081c8ef7cfa6244
bruno1923 <u201914974@upc.edu.pe>
2022-11-16 12:15:36 -0500

    feat(profiles): update Resources

--- HealthyPets.API/Profiles/Resource/ClientResource.cs
+++ HealthyPets.API/Profiles/Resource/ClientResource.cs
@@ -1,7 +1,24 @@
-﻿namespace HealthyPets.API.Profiles.Resource;
+﻿using HealthyPets.API.Patients.Domain.Model;
+
+namespace HealthyPets.API.Profiles.Resource;
 
 public class ClientResource
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name{ get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Address { get; set; }
+    
+    public long Ruc { get; set; }
+    
+    public long Phone { get; set; }
+    
+    public Pet Pet{ get; set; }
+    
+    public long UserId { get; set; }
+
 }
--- HealthyPets.API/Profiles/Resource/SaveClientResource.cs
+++ HealthyPets.API/Profiles/Resource/SaveClientResource.cs
@@ -1,9 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using HealthyPets.API.Patients.Domain.Model;
 
 namespace HealthyPets.API.Profiles.Resource;
 
 public class SaveClientResource
 {
     [Required]
+    [MaxLength(200)]
     public string Name { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string LastName { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Email { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Adress { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Ruc { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Phone { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public Pet Pet { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public long UserId { get; set; }
 }
+
+
