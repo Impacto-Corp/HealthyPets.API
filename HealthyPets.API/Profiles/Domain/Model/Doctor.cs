dd963729a444e26275f640ab1635ca2ed1a63592
bruno1923 <u201914974@upc.edu.pe>
2022-11-16 12:24:59 -0500

    feat(profiles): added more properties(Doctor)

--- HealthyPets.API/Profiles/Domain/Model/Doctor.cs
+++ HealthyPets.API/Profiles/Domain/Model/Doctor.cs
@@ -5,4 +5,10 @@ public class Doctor
     //Properties
     public long Id { get; set; }
     public string Name{ get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public long Phone { get; set; }
 }
