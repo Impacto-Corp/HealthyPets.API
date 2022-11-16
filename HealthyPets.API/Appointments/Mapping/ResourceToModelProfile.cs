43b0d0d8c1090e926e869bb9171e3313b750d846
RauVR <u201714049@upc.edu.pe>
2022-11-16 11:54:59 -0500

    feat(Appointment): Class ResourceToModelProfile updated.

--- HealthyPets.API/Appointments/Mapping/ResourceToModelProfile.cs
+++ HealthyPets.API/Appointments/Mapping/ResourceToModelProfile.cs
@@ -9,5 +9,6 @@ public class ResourceToModelProfile : Profile
     public ResourceToModelProfile()
     {
         CreateMap<SaveAppointmentResource, Appointment>();
+        CreateMap<SaveExamResource, Exam>();
     }
 }
