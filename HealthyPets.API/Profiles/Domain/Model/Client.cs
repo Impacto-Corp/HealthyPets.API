0bebabed14e1b2cd09de58bca246609f9ad10060
bruno1923 <u201914974@upc.edu.pe>
2022-11-16 11:54:03 -0500

    feat(profiles): update Client.cs

--- HealthyPets.API/Profiles/Domain/Model/Client.cs
+++ HealthyPets.API/Profiles/Domain/Model/Client.cs
@@ -1,13 +1,35 @@
-﻿using HealthyPets.API.Patients.Domain.Model;
+﻿using HealthyPets.API.Appointments.Domain.Model;
+using HealthyPets.API.Patients.Domain.Model;
 
 namespace HealthyPets.API.Profiles.Domain.Model;
 
 public class Client
 {
+    public Client()
+    {
+        Appointments = new List<Appointment>();
+    }
     //Properties
     public int Id { get; set; }
     public string Name{ get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Address { get; set; }
+    
+    public long Ruc { get; set; }
+    
+    public long Phone { get; set; }
+    
     public Pet Pet{ get; set; }
     
     public long UserId { get; set; }
-}
+    
+    
+    //Relationships
+    public IList<Appointment> Appointments { get; set; }
+
+    }
+   
