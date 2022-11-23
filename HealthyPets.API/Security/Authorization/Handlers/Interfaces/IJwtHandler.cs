771ae0a6d7b8d2407c5455c2c3ca8df2b5f0b49f
Lynn Jeerferzon <81722082+LynnJ-Meza@users.noreply.github.com>
2022-11-23 05:17:50 -0500

    feat(security):implemented token handler.

---
+++ HealthyPets.API/Security/Authorization/Handlers/Interfaces/IJwtHandler.cs
@@ -0,0 +1,9 @@
+﻿using HealthyPets.API.Security.Domain.Model;
+
+namespace HealthyPets.API.Security.Authorization.Handlers.Interfaces;
+
+public interface IJwtHandler
+{
+    public string GenerateToken(User user);
+    public int? ValidateToken(string token);
+}
