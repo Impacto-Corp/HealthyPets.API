159a4292349dd442356289336f206eed614a93e8
Lynn Jeerferzon <81722082+LynnJ-Meza@users.noreply.github.com>
2022-11-23 06:01:26 -0500

    feat(security):added jwt middleware.

---
+++ HealthyPets.API/Security/Authorization/Middleware/JwtMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using HealthyPets.API.Security.Authorization.Handlers.Interfaces;
+using HealthyPets.API.Security.Authorization.Setting;
+using HealthyPets.API.Security.Domain.Services;
+
+namespace HealthyPets.API.Security.Authorization.Middleware;
+
+public class JwtMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly AppsSettings _appSettings;
+
+    public JwtMiddleware(RequestDelegate next, AppsSettings appSettings)
+    {
+        _next = next;
+        _appSettings = appSettings;
+    }
+
+    public async Task Invoker(
+        HttpContext context,
+        IUserService userService,
+        IJwtHandler handler)
+    {
+        var token = context.Request.Headers["Authorization"]
+            .FirstOrDefault()?
+            .Split(" ")
+            .Last();
+
+        var userId = handler.ValidateToken(token);
+
+        if (userId != null)
+            context.Items["User"] = await userService.GetByIdAsync(userId.Value);
+
+        await _next(context);
+
+    }
+}
